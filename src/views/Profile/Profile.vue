d565006b3f5decebb67400fd036959c19fd05609
TKhai55 <thienkhaiak5555@gmail.com>
2023-12-24 16:37:14 +0700

    done UI profile information

--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -11,40 +11,64 @@
         </div>
 
         <div class="information-tab" v-if="currentTab === 'information'">
-            <div class="profile-section">
+            <div class="left-column">
                 <div class="avatar-container">
-                    <img src="https://external-preview.redd.it/Ot_7l56sKeF7k7yF33xE4rsVUSC3ZtNfvLrr-o66KLo.jpg?auto=webp&s=d6bf7543974bb7bdf2536d2d80d0e41debd23777" alt="">
+                    <img src="https://external-preview.redd.it/Ot_7l56sKeF7k7yF33xE4rsVUSC3ZtNfvLrr-o66KLo.jpg?auto=webp&s=d6bf7543974bb7bdf2536d2d80d0e41debd23777"
+                        alt="">
                 </div>
-                <div class="info-container">
-                    <div class="infor-input">
-                        <label for="name">Email:</label>
-                        <input type="text" id="name" v-model="email" disabled/>
+                <p>{{ email }}</p>
+            </div>
+            <div class="right-column">
+                <div class="profile-section">
+                    <div class="button-container-profile">
+                        <button>Edit Profile</button>
                     </div>
-                    <div class="infor-input">
-                        <label for="name">Name:</label>
-                        <input type="text" id="name" v-model="name" disabled/>
+
+                    <div class="info-container">
+                        <div class="infor-input">
+                            <label for="name">Name:</label>
+                            <input class="i-text" type="text" id="name" v-model="name" />
+                        </div>
+                        <div class="infor-input">
+                            <label for="name">Age:</label>
+                            <input class="i-num" type="number" id="name" v-model="age" min="0" />
+                        </div>
+                        <div class="infor-input">
+                            <label for="gender">Gender:</label>
+                            <select id="gender" v-model="gender" class="i-num">
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </div>
+                        <div class="infor-input">
+                            <label for="name">Height:</label>
+                            <input class="i-num" type="number" id="height" v-model="height" min="0" />
+                            <span>( cm )</span>
+                        </div>
+                        <div class="infor-input">
+                            <label for="name">Weight:</label>
+                            <input class="i-num" type="number" id="weight" v-model="weight"  min="0"/>
+                            <span>( kg )</span>
+
+                        </div>
+                        <div class="infor-input">
+                            <label for="name">Activity Level:</label>
+                            <input class="i-text" type="text" v-model="activity" disabled />
+                        </div>
+                        <div class="infor-input">
+                            <label for="name">BMI:</label>
+                            <input class="i-num" type="number" v-model="bmi" disabled />
+                            <span style="color: rgb(28, 196, 28);">( {{ health }} )</span>
+                        </div>
+                        <div class="infor-input">
+                            <label for="name">Daily Calories:</label>
+                            <input class="i-num" type="number" v-model="calories" disabled />
+                            <span>( Calories/day )</span>
+                        </div>
                     </div>
-                    <div class="infor-input">
-                        <label for="name">Age:</label>
-                        <input type="number" id="name" v-model="age" disabled/>
-                    </div>
-                    <div class="infor-input">
-                        <label for="name">Height:</label>
-                        <input type="number" id="name" v-model="height" disabled/>
-                        <span class="unit">cm</span>
-                    </div>
-                    <div class="infor-input">
-                        <label for="name">Weight:</label>
-                        <input type="number" id="name" v-model="weight" disabled/>
-                        <span class="unit">kg</span>
-                    </div>
-                    <div class="infor-input">
-                        <label for="name">BMI:</label>
-                        <input type="number" id="name" v-model="bmi" disabled/>
-                    </div>
-                   
                 </div>
             </div>
+
         </div>
 
         <div class="manageMeal-tab" v-if="currentTab === 'manageMeal'">
@@ -61,9 +85,13 @@ const currentTab = ref('information');
 const name = ref('John Doe');
 const email = ref('john.doe@example.com');
 const age = ref(25);
-const height = ref('175');
-const weight = ref('70');
-const bmi = ref('23.5');
+const gender = ref('male');
+const health = ref('Normal');
+const activity = ref('Exercise 1-3 times/week');
+const height = ref(175);
+const weight = ref(70);
+const bmi = ref(23.5);
+const calories = ref(2350);
 
 const changeTab = (tab) => {
     currentTab.value = tab;
